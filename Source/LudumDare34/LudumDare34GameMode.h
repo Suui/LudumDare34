b6d8023aa73e78c1d939766f1a532cde368bba1f
Suui <suui.gd@gmail.com>
2015-12-12 12:00:12 +0100

    Initial stage

---
+++ Source/LudumDare34/LudumDare34GameMode.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameMode.h"
+#include "LudumDare34GameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class LUDUMDARE34_API ALudumDare34GameMode : public AGameMode
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
